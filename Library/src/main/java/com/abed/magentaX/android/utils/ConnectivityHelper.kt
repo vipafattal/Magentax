67bd25eefc45b4f1d7a016711ae96bc9beaea10b
vipafattal <vipafattal@gmail.com>
2019-12-06 22:30:53 +0200

    ConnectivityHelper Deprecation fixed

--- Library/src/main/java/com/abed/magentaX/android/utils/ConnectivityHelper.kt
+++ Library/src/main/java/com/abed/magentaX/android/utils/ConnectivityHelper.kt
@@ -2,20 +2,43 @@ package com.abed.magentaX.android.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.IntRange
 import androidx.annotation.RequiresPermission
 import com.abed.magentaX.android.os.MagentaX
+
 
 object ConnectivityHelper {
 
     @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
-    fun isConnectedToNetwork(): Boolean {
-        val connectivityManager =
-            MagentaX.application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    fun isConnectedToNetwork(): Boolean = getConnectionType() != 0
 
-        val isConnected: Boolean
-        val activeNetwork = connectivityManager.activeNetworkInfo
-        isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting
 
-        return isConnected
+    //https://stackoverflow.com/a/53243938/6470661
+    @IntRange(from = 0, to = 2)
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+    fun getConnectionType(): Int {
+        var result = 0
+        (MagentaX.application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?)?.run {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getNetworkCapabilities(activeNetwork)?.run {
+                    result = when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> 2
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> 1
+                        else -> 0
+                    }
+                }
+
+            } else {
+                @Suppress("DEPRECATION")
+                result = when (activeNetworkInfo?.type) {
+                    ConnectivityManager.TYPE_WIFI -> 2
+                    ConnectivityManager.TYPE_MOBILE -> 1
+                    else -> 0
+                }
+            }
+        }
+        return result
     }
 }
--- Library/src/main/java/com/abed/magentaX/android/utils/SDKSupport.kt
+++ Library/src/main/java/com/abed/magentaX/android/utils/SDKSupport.kt
@@ -4,8 +4,13 @@ import android.os.Build
 import com.abed.magentaX.standard.lambda.unitFun
 
 
-inline fun sdkSupport(SDKVersion: Int, codeBlock: unitFun) {
-    if (Build.VERSION.SDK_INT >= SDKVersion) {
+inline fun sdkSupport(sdk: Int, codeBlock: unitFun) {
+    if (Build.VERSION.SDK_INT >= sdk)
         codeBlock()
-    }
+}
+inline fun sdkSupport(sdk: Int, codeBlock: unitFun, orElse:unitFun) {
+    if (Build.VERSION.SDK_INT >= sdk) 
+        codeBlock()
+    else
+        orElse()
 }
