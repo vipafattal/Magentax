0029be8f93df49805650732125ba0fdbf0d1f02d
vipafattal <vipafattal@gmail.com>
2019-12-09 22:21:15 +0200

    Remove Fragment functions added

--- Library/src/main/java/com/abed/magentaX/android/fragments/RemoveFragment.kt
+++ Library/src/main/java/com/abed/magentaX/android/fragments/RemoveFragment.kt
@@ -4,6 +4,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 
-fun FragmentManager.removeFragment(frag: Fragment) {
-    beginTransaction().remove(frag).commit()
+fun FragmentManager.removeFragment(frag: Fragment?) {
+    if (frag != null)
+        beginTransaction().remove(frag).commit()
+
+}
+
+fun Fragment.removeFragment() {
+        fragmentManager?.removeFragment(this)
+}
+
+fun FragmentManager.removeFragmentByTag(tag:String) :Boolean {
+    val fragment = findFragmentByTag(tag)
+    removeFragment(fragment)
+    return fragment != null
 }
