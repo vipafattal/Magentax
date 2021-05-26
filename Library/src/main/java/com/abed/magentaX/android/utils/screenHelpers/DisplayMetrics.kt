d942657287bd9733b52a2dfcc8e31f9e2f92cf71
vipafattal <vipafattal@gmail.com>
2021-05-26 18:16:42 +0300

    dependencies upgrade & DisplayMetrics.kt java introp

--- Library/src/main/java/com/abed/magentaX/android/utils/screenHelpers/DisplayMetrics.kt
+++ Library/src/main/java/com/abed/magentaX/android/utils/screenHelpers/DisplayMetrics.kt
@@ -10,40 +10,36 @@ import com.abed.magentaX.android.resoures.appResources
  * Defines a scale factor to use for scaling what'imageItem displayed
  */
 // Set the screen width and height
+class DisplayMetrics {
 
-private var dm: DisplayMetrics = application.resources.displayMetrics
+    companion object {
+        private var dm: DisplayMetrics = application.resources.displayMetrics
 
-val screenWidth: Int = dm.widthPixels
-val screenHeight: Int = dm.heightPixels
+        @JvmField
+        val screenWidth: Int = dm.widthPixels
 
+        @JvmField
+        val screenHeight: Int = dm.heightPixels
 
-fun density() = dm.density
+        @JvmStatic
+        fun density() = dm.density
 
-fun dp(px: Float): Float =
-        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px,
-                application.resources.displayMetrics)
+        @JvmStatic
+        fun dp(px: Float): Float =
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px,
+                        application.resources.displayMetrics)
 
-fun dp(px: Int):Int =
-        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px.toFloat(),
-                application.resources.displayMetrics).toInt()
+        @JvmStatic
+        fun dp(px: Int): Int =
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px.toFloat(),
+                        application.resources.displayMetrics).toInt()
 
-fun pixelsToSp(px: Float): Float {
-    val scaledDensity = appResources.displayMetrics.scaledDensity
-    return px / scaledDensity
-}
-
-/*    fun density(): Float {
-        val x = dm.widthPixels
-        val y = dm.heightPixels
-        if (x < y) {
-            val xr = dm.widthPixels / 320.0f
-            val yr = dm.heightPixels / 480.0f
-            return Math.initMin(xr, yr)
-        } else {//landscape
-            val xr = dm.widthPixels / 480.0f
-            val yr = dm.heightPixels / 320.0f
-            return Math.initMin(xr, yr)
+        @JvmStatic
+        fun pixelsToSp(px: Float): Float {
+            val scaledDensity = appResources.displayMetrics.scaledDensity
+            return px / scaledDensity
         }
-    }*/
+    }
+}
 
 
